407afa5c49b271f0233cabd60e4872b7988ea338
Sebastian Tylka <113390513+SaCha802@users.noreply.github.com>
2024-01-31 12:06:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+let sku = 0;
+let token = 0;
+
 document.addEventListener('DOMContentLoaded', function() {
   // Parse URL parameters
   const params = new URLSearchParams(window.location.search);
-  const message = params.get('message');
+  sku = params.get('sku');
+  token = params.get('token');
 
   // Display message in output div
   if (message) {
@@ -10,3 +14,41 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('output').innerText = 'No message found in URL parameters.';
   }
 });
+
+async function sendRequest(sku, token) {
+  try {
+    const response = await axios.post(
+      'https://corsproxy.io/?https://connect.squareup.com/v2/catalog/search-catalog-items',
+      {
+        'text_filter': sku,
+        "limit": 1,
+      },
+      {
+        headers: {
+          'Square-Version': '2023-07-20',
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    console.log(response.data);
+    for (var item of response.data.items) {
+      for (var variations of item.item_data.variations) {
+        if (variations.id == response.data.matched_variation_ids) {
+          var product_name = item.item_data.name;
+          var product_var = variations.item_variation_data.name;
+          var price = variations.item_variation_data.price_money.amount;
+          price /= 100;
+          var sku = variations.item_variation_data.sku;
+          return [product_name, product_var, price, sku];
+        }
+      }
+    }
+    // If the loop doesn't find any matching variation, you may want to handle this case as well.
+    throw new Error("No matching variation found.");
+  } catch (error) {
+    // Display the error in the responseContainer div
+    alert('No matching SKU found.');
+    throw error;
+  }
+}
